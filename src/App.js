2ba918e11cbae1aab69532840ed3e90e89dce7ce
Chris Cooper <cjchrisjcooper@gmail.com>
2024-02-13 23:46:43 -0700

    renamed folders

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
-import Header from "./Components/Header/Header";
-import Main from "./Components/Main/Main";
-import { currentWeather } from "./Components/utils/constants";
-import Footer from "./Components/Footer/Footer";
-import ModalWithForm from "./Components/ModalWithForm/ModalWithForm";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import ModalWithForm from "./components/ModalWithForm/ModalWithForm";
 import { useEffect, useState } from "react";
-import ItemModal from "./Components/PreviewModal/ItemModal";
+import ItemModal from "./components/PreviewModal/ItemModal";
 import {
   GetWeatherForcast,
   parseWeatherData,
-} from "./Components/utils/WeatherApi";
+} from "./components/utils/WeatherApi";
 function App() {
   const [activeModal, setActiveModal] = useState("");
   const [selectCard, setSelectedCard] = useState({});
--- src/Components/WeatherCard/WeatherCard.js
+++ src/Components/WeatherCard/WeatherCard.js
@@ -2,70 +2,70 @@ import "../Main/Main.css";
 
 const weatherOptions = [
   {
-    url: require("../../Images/day/Sunny.svg").default,
+    url: require("../../images/day/Sunny.svg").default,
     day: true,
     type: "sunny",
   },
   {
-    url: require("../../Images/day/Cloudy.svg").default,
+    url: require("../../images/day/Cloudy.svg").default,
     day: true,
     type: "cloudy",
   },
-  { url: require("../../Images/day/fog.svg").default, day: true, type: "fog" },
+  { url: require("../../images/day/fog.svg").default, day: true, type: "fog" },
   {
-    url: require("../../Images/day/Rain.svg").default,
+    url: require("../../images/day/Rain.svg").default,
     day: true,
     type: "rain",
   },
   {
-    url: require("../../Images/day/Snow.svg").default,
+    url: require("../../images/day/Snow.svg").default,
     day: true,
     type: "snow",
   },
   {
-    url: require("../../Images/night/cloudy-night.svg").default,
+    url: require("../../images/night/cloudy-night.svg").default,
     day: false,
     type: "cloudy-night",
   },
   {
-    url: require("../../Images/night/fog-night.svg").default,
+    url: require("../../images/night/fog-night.svg").default,
     day: false,
     type: "fog-night",
   },
   {
-    url: require("../../Images/night/night.svg").default,
+    url: require("../../images/night/night.svg").default,
     day: false,
     type: "night",
   },
   {
-    url: require("../../Images/night/rainy-night.svg").default,
+    url: require("../../images/night/rainy-night.svg").default,
     day: false,
     type: "rainy-night",
   },
   {
-    url: require("../../Images/night/snow-night.svg").default,
+    url: require("../../images/night/snow-night.svg").default,
     day: false,
     type: "snow-night",
   },
   {
-    url: require("../../Images/night/storm-night.svg").default,
+    url: require("../../images/night/storm-night.svg").default,
     day: false,
     type: "storm-night",
   },
 ];
 
 const WeatherCard = ({ day, type, weatherTemp = "" }) => {
-  const imageSrc = weatherOptions.filter((i) => {
+  const imagesrc = weatherOptions.filter((i) => {
     if (i.day === day && i.type === type) {
       return i;
     }
   });
 
-  const imageSrcUrl = imageSrc[0].url || "";
+  const imagesrcUrl = imagesrc[0].url || "";
   return (
     <section id="weather" className="main__weather">
       <p className="main-weather-info">{weatherTemp}F°</p>
-      <img src={imageSrcUrl} className="main-weather-image"></img>
+      <img src={imagesrcUrl} className="main-weather-image"></img>
     </section>
   );
 };
