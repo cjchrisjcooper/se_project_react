7ae70c10d739e015a752096423916acdffdc7f49
Chris Cooper <cjchrisjcooper@gmail.com>
2024-06-29 22:56:21 -0700

    SideBar html in place

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -8,12 +8,20 @@ const SideBar = () => {
 
   return (
     <div className="sideBar">
-      <img
-        src={currentUser.avatar}
-        className="header__avatar-image sidebar__avatar-image"
-        alt="Avatar"
-      ></img>
-      <p className="header__name">{currentUser.name}</p>
+      <div className="sideBar__profile">
+        <img
+          src={currentUser.avatar}
+          className="header__avatar-image sidebar__avatar-image"
+          alt="Avatar"
+        ></img>
+        <p className="header__name">{currentUser.name}</p>
+      </div>
+      <div className="sideBar__edit-profile-section">
+        <button className="sideBar__button">Change Profile data</button>
+      </div>
+      <div className="sideBar__log-out-section">
+        <button className="sideBar__button">Log Out</button>
+      </div>
     </div>
   );
 };
