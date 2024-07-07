dc38ac71a483debc51fc85f949283cda2dfcda47
Chris Cooper <cjchrisjcooper@gmail.com>
2024-07-07 10:26:21 -0700

    Removed files for renaming to uppercase folders

--- src/components/RemovedFilesByCase/Profile.js
+++ src/components/RemovedFilesByCase/Profile.js
@@ -1,5 +1,5 @@
 import ClothesSection from "../ClothesSection/ClothesSection";
-import SideBar from "../SideBar/SideBar";
+import SideBar from "./SideBar";
 import "./Profile.css";
 
 const Profile = ({
